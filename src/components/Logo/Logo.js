90c59de94c2c0bd0c2a021ac37d95026920524c2
choisuhyeok1255 <csh300029@gmail.com>
2021-03-18 23:51:59 +0900

    feat: logo component finish

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactComponent as ColorLogo} from 'assets/coupangLogos/logo-colorful.svg';
+import { ReactComponent as BlackLogo} from 'assets/coupangLogos/logo-black.svg';
+import { ReactComponent as MonoLogo} from 'assets/coupangLogos/logo-mono.svg';
+
+const Logo = ( {type} ) => {
+  return type === 'colorful' ? <ColorLogo/> : type === 'mono' ? <MonoLogo/> : <BlackLogo/>
+}
+
+export default Logo;
